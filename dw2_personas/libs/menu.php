4c9ed297f4fe4fd953d128c80c75c3e2d5b9de63
Nathalia Schneider <81602411+nathischneider03@users.noreply.github.com>
2022-06-09 01:59:19 -0400

    Delete dw2_personas directory

--- dw2_personas/libs/menu.php
+++
@@ -1,6 +0,0 @@
-<nav class="navbar navbar-light bg-light">
-  <p class="form-inline">
-    <a class="btn btn-outline-success" href="/dw2_personas/ciudades/" >Ciudades</a>
-    <a class="btn btn-outline-success" href="/dw2_personas/personas/"  >Personas</a>
-  </p>
-</nav>
